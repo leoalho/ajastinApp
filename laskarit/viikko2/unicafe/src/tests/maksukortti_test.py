2bae3b4b24fc08d30053667ebea32070279149ba
Leo <leo.a.alho@gmail.com>
2022-11-11 15:46:21 +0200

    Tehtävä 6: Lisätty testit unicafe repositorioon

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_on_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa(self):
+        self.maksukortti.lataa_rahaa(1000)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+    
+    def test_rahaa_ottaessa_saldo_pienenee_jos_rahaa_riittavasti(self):
+        self.maksukortti.ota_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_rahaa_ottaessa_saldo_ei_pienene_jos_rahaa_ei_riittavasti(self):
+        self.maksukortti.ota_rahaa(2000)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_ota_rahaa_palauttaa_true_jos_rahat_riittaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+    
+    def test_ota_rahaa_palauttaa_false_jos_rahat_eivat_riita(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(2000), False)
